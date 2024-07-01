d1bad3213ebac6c34170da1010a632626e8baaa9
Yoseph Kedir <yoseph.kedir10@gmail.com>
2024-07-02 00:37:31 +0300

    Time: 50 ms (91.69%), Space: 51.5 MB (40.46%) - LeetHub

--- 0268-missing-number/0268-missing-number.js
+++ 0268-missing-number/0268-missing-number.js
@@ -3,10 +3,11 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
-    nums.sort((a,b) => a - b)
-    for(i = 0; i <= nums.length; i++){
-        if(i !== nums[i]){
-            return i
+        let res = nums.length;
+        
+        for (let i = 0; i < nums.length; i++) {
+            res += i - nums[i];
         }
-    }
+        
+        return res;
 };
