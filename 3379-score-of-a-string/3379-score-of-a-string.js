1bb091050f05908d62d378eec11a997105a67e1f
Yoseph Kedir <yoseph.kedir10@gmail.com>
2024-07-02 20:02:06 +0300

    Time: 50 ms (94.3%), Space: 49.8 MB (58.87%) - LeetHub

--- 3379-score-of-a-string/3379-score-of-a-string.js
+++ 3379-score-of-a-string/3379-score-of-a-string.js
@@ -4,8 +4,8 @@
  */
 var scoreOfString = function(s) {
     let sum = 0
-    for(i = 0; i < s.length - 1; i++){
-        sum += (Math.abs(s.charCodeAt(i) - s.charCodeAt(i + 1)))
+    for(i = 1; i < s.length; i++){
+        sum += (Math.abs(s.charCodeAt(i - 1) - s.charCodeAt(i)))
     }
     return sum
 };
