c8d71209839d44ca0483eaf8610ebede59c1c1a8
Yoseph Kedir <yoseph.kedir10@gmail.com>
2024-07-04 15:00:06 +0300

    Time: 63 ms (6.95%), Space: 17.6 MB (17.28%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s = list(s)
+        t = list(t)
+        s.sort()
+        t.sort()
+        if s == t:
+            return True
+        return False
