a7b9ea18985f667ca1cfbf1c4cd4393b43e2d328
Yoseph Kedir <yoseph.kedir10@gmail.com>
2024-07-04 14:44:58 +0300

    Time: 65 ms (96.15%), Space: 57.2 MB (32.57%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.js
+++ 0344-reverse-string/0344-reverse-string.js
@@ -3,5 +3,10 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-    return s.reverse()
+  let i = 0, j = s.length - 1;
+  while (i < j) {
+    [s[i], s[j]] = [s[j], s[i]];
+    i++;
+    j--;
+  }
 };
