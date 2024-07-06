a99b463f29d2bbe878550a4230dc82242ffada43
Yoseph Kedir <yoseph.kedir10@gmail.com>
2024-07-07 01:06:43 +0300

    Time: 27 ms (94.23%), Space: 16.6 MB (21.46%) - LeetHub

--- 0782-jewels-and-stones/0782-jewels-and-stones.py
+++ 0782-jewels-and-stones/0782-jewels-and-stones.py
@@ -1,7 +1,3 @@
 class Solution:
-    def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        count = 0
-        for i in stones:
-            if i in jewels:
-                count += 1
-        return count
+    def numJewelsInStones(self, J, S):
+        return sum(map(J.count, S))
