45ddfaa5959824978c0137d2b079a017332637f1
Yoseph Kedir <yoseph.kedir10@gmail.com>
2024-12-24 11:42:13 +0300

    Time: 51 ms (73.96%), Space: 49.4 MB (40.76%) - LeetHub

---
+++ 2761-array-reduce-transformation/2761-array-reduce-transformation.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    return nums.reduce((acc, curr) => fn(acc, curr), init);
+    
+};
