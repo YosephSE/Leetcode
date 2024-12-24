190587f4e0f033cdc2037abe8edef8db5011a733
Yoseph Kedir <yoseph.kedir10@gmail.com>
2024-12-24 09:02:11 +0300

    Time: 62 ms (39.27%), Space: 51.8 MB (43.42%) - LeetHub

---
+++ 2789-counter-ii/2789-counter-ii.js
@@ -0,0 +1,20 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let count = init;
+    return {
+        increment: () => {return ++count},
+        decrement: () => {return --count},
+        reset: () => {return count = init}
+    }
+    
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
