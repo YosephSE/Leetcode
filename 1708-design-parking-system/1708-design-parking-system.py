b6462733efabc57848522cb1360da642932a6019
Yoseph Kedir <yoseph.kedir10@gmail.com>
2024-12-28 14:48:50 +0300

    Time: 7 ms (91.53%), Space: 18.5 MB (6.78%) - LeetHub

--- 1708-design-parking-system/1708-design-parking-system.py
+++ 1708-design-parking-system/1708-design-parking-system.py
@@ -1,13 +1,11 @@
 class ParkingSystem:
 
     def __init__(self, big: int, medium: int, small: int):
-        self.spots = [big, medium, small]
-
-        
+        self.spots = {1: big, 2: medium, 3: small}
 
     def addCar(self, carType: int) -> bool:
-        if self.spots[carType - 1] > 0:
-            self.spots[carType - 1] -= 1
+        if self.spots[carType] > 0:
+            self.spots[carType] -= 1
             return True
         return False
 
