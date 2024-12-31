01b0f54c88d940b032a0d8fe214facc154a282ba
Yoseph Kedir <yoseph.kedir10@gmail.com>
2024-12-31 19:31:39 +0300

    Time: 0 ms (100%), Space: 18.9 MB (8.33%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,11 +1,10 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        hash_map = {}  
-        for i, num in enumerate(nums):
-            diff = target - num 
-            if diff in hash_map:
-                return [hash_map[diff], i]  
-            hash_map[num] = i
-        
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        map = {}
+        for i in range(len(nums)):
+            comp = target - nums[i]
+            if comp in map:
+                return [map[comp], i]
+            map[nums[i]] = i
 
-    
+        
