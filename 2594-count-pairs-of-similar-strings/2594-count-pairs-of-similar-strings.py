07725e93a221fce5cabc7f0e9bb928d26dfbfeda
Yoseph Kedir <yoseph.kedir10@gmail.com>
2025-01-02 19:31:53 +0300

    Time: 11 ms (88.86%), Space: 18.1 MB (5.16%) - LeetHub

--- 2594-count-pairs-of-similar-strings/2594-count-pairs-of-similar-strings.py
+++ 2594-count-pairs-of-similar-strings/2594-count-pairs-of-similar-strings.py
@@ -1,10 +1,10 @@
 class Solution:
     def similarPairs(self, words: List[str]) -> int:
+        signature_count = Counter()
+        for word in words:
+            signature_count[''.join(sorted(set(word)))] += 1
         count = 0
-        for i in range(len(words)):
-            for j in range(i + 1, len(words)):
-                if set(words[i]) == set(words[j]):
-                    count += 1
+        for freq in signature_count.values():
+            count += freq * (freq - 1) // 2
         return count
-
         
