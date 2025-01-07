e55129657ecb2b2633ae0db449fa0f7e91b02a35
Yoseph Kedir <yoseph.kedir10@gmail.com>
2025-01-07 19:49:49 +0300

    Time: 0 ms (100%), Space: 17.9 MB (11.31%) - LeetHub

---
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.py
@@ -0,0 +1,10 @@
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        res = 0
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] == nums[j]:
+                    res += 1
+        return res
+
+        
