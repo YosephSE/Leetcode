73a07ccf0ea10ef60f81eb939d10e16bcce0aa3e
Yoseph Kedir <yoseph.kedir10@gmail.com>
2025-01-14 09:18:00 +0300

    Time: 9 ms (77.42%), Space: 12.5 MB (38.71%) - LeetHub

--- 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.py
+++ 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.py
@@ -1,10 +1,22 @@
 class Solution(object):
     def findThePrefixCommonArray(self, A, B):
-        map = {}
+        mapA, mapB = {}, {}
         common = []
+        val = 0
         for i in range(len(A)):
-            val = len(set(A[:i + 1]).intersection(set(B[:i + 1])))
+            mapA[A[i]] = 1
+            mapB[B[i]] = 1
+            if A[i] == B[i]:
+                val += 1
+            else:
+                if mapB.get(A[i], 0) == 1:
+                    val += 1
+                if mapA.get(B[i], 0) == 1:
+                    val += 1
             common.append(val)
+        return common
+
+            
         
         return common
         
