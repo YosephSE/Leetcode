fd5a4b209c9741f9cae09f189b00fb9974226e25
Yoseph Kedir <yoseph.kedir10@gmail.com>
2025-01-15 17:32:22 +0300

    Time: 27 ms (44.92%), Space: 18.2 MB (9.87%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        res = defaultdict(list)
+
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            res[tuple(count)].append(s)
+        return res.values()
